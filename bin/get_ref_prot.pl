26f23371658844a6b359ca1dfcaab018a28f7332
mirimia <mirimia@gmail.com>
2019-08-28 19:17:16 +0200

    bug in filehandle for input that results in empty files for ref proteins

--- bin/get_ref_prot.pl
+++ bin/get_ref_prot.pl
@@ -16,19 +16,19 @@ my %size;
 my %lsize;
 
 #ENSP00000379387	ENSG00000185219	1031
-open (INONE,"$i1");
-while (<INTWO>){
-	chomp($_); 
-	@line=split(/\t/,$_);
-	if (!$longest{$line[1]}){
-		$prot{$line[1]}=1;
-		$lsize{$line[1]}=$line[2];
-		$longest{$line[1]}=$line[0];
-	}
-	elsif ($line[2]>$lsize{$line[1]}){
-		$lsize{$line[1]}=$line[2];
-		$longest{$line[1]}=$line[0];
-	}
+open (INONE,"$i1") || die "Cannot open $i1\n";
+while (<INONE>){
+    chomp($_); 
+    @line=split(/\t/,$_);
+    if (!$longest{$line[1]}){
+	$prot{$line[1]}=1;
+	$lsize{$line[1]}=$line[2];
+	$longest{$line[1]}=$line[0];
+    }
+    elsif ($line[2]>$lsize{$line[1]}){
+	$lsize{$line[1]}=$line[2];
+	$longest{$line[1]}=$line[0];
+    }
 }
 close (INFILE);
 
@@ -38,10 +38,10 @@ my $t;
 my $sc=scalar(@keys);
 open (OUT,">$out");
 foreach $el (@keys){
-	if ($longest{$el})  {
-		$t=$longest{$el};
-		print OUT "$el\t$longest{$el}\n";
-	}
+    if ($longest{$el})  {
+	$t=$longest{$el};
+	print OUT "$el\t$longest{$el}\n";
+    }
 }
 
 
