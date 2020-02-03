3e3a37abdc68e5ac3bc6b7dbfefed949533c51a3
YamileM <38042457+YamileM@users.noreply.github.com>
2020-02-03 12:39:07 +0100

    Script to optimize number of realn exons

---
+++ bin/Split_realn_exons.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+use warnings;
+#use strict;
+
+my$p=1000;
+my $dir=$ARGV[0];
+$p=$ARGV[1];
+
+$file=$dir."/tmp.txt";
+`cat $dir/exons_to_realign_part_* > $file`;
+`rm $dir/exons_to_realign_part_*`;
+
+$h=0;
+$c=0;
+$t=1;
+open (FILE, "$file");
+while (<FILE>){
+	chomp($_);
+	if ($_=~/CID/) { $h=$_; }
+	else { $c++; 
+	if ($c==1){
+		$of=$dir."/exons_to_realign_part_".$t.".txt";
+		open (OUT, ">>$of");
+		if (!$hd{$t}){
+			print OUT "$h\n";
+			$hd{$t}=1;
+		}
+		print OUT "$_\n";
+	}
+	elsif ($c>$p){
+		$t++; 
+		$of=$dir."/exons_to_realign_part_".$t.".txt";
+		open (OUT, ">>$of");
+		if (!$hd{$t}){
+			print OUT "$h\n";
+			$hd{$t}=1;
+		}
+		print OUT "$_\n";
+		$c=0;
+	}
+	else {
+		$of=$dir."/exons_to_realign_part_".$t.".txt";
+		open (OUT, ">>$of");
+		print OUT "$_\n";	
+	}	
+	}
+
+}
+
+
+
