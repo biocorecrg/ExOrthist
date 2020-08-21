aebda0c687d08a01359f4cd247f0a1cc51945e50
mirimia <mirimia@gmail.com>
2020-08-21 13:38:16 +0200

    formal changes to Get_membership_score_ECL.pl

--- bin/Get_membership_score_ECL.pl
+++ bin/Get_membership_score_ECL.pl
@@ -1,29 +1,27 @@
 #!/usr/bin/perl
-#use warnings;
-#use strict;
+use warnings;
+use strict;
 
-my $i1=$ARGV[0]; #EXONS_DB folder
-my $i2=$ARGV[1]; ##Best exon hits filtered
-my $i3=$ARGV[2]; ##Exon clusters
-my $out=$ARGV[3]; ##Exon clusters with membership score
+my $exons_db_folder=$ARGV[0]; #EXONS_DB folder
+my $infile1=$ARGV[1]; ##Best exon hits filtered
+my $infile2=$ARGV[2]; ##Exon clusters
+my $outfile=$ARGV[3]; ##Exon clusters with membership score
 my ($id1, $as, $ts, $l, $id2, $n);
 
-$ef=`ls $i1/*/*_prot_exons_overlap.txt`;
+$ef=`ls $exons_db_folder/*/*_prot_exons_overlap.txt`;
 @f=split(/\n/,$ef);
 for ($k=0; $k<scalar(@f); $k++){
     open (GCL, $f[$k]);
-    #print "$f[$k]\n";
     #OV_EX_Hs2_23	ENSG00000000419	50948629-50948662
     while (<GCL>){
 	chomp($_);
-	#print "$_\n";
 	@l=split(/\t/,$_);
 	$oid{$l[1]."\t".$l[2]}=$l[0];
     }	
 }
 
 #ENSG00000203814	chr1:149783236-149783260:-	ENSMUSG00000069308	chr13:21789024-21789060:+	Hs2	Mm2	1.7204
-open (BH, $i2);
+open (BH, $infile1) || die "It cannot open the best exon hits filtered ($infile1)\n";
 while (<BH>){
     chomp($_);
     @l=split(/\t/,$_);
@@ -31,16 +29,16 @@ while (<BH>){
     @t2=split(/\:/,$l[3]);
     $r1=$oid{$l[0]."\t".$t1[1]};
     $r2=$oid{$l[2]."\t".$t2[1]};
-    #print "$r1\t$r2\n";
     $id1=$r1."\t".$r2;
     $id2=$r2."\t".$r1;
     $pair{$id1}=1;
     $pair{$id2}=1;
 }
+close BH;
 
 #GF0000007.001	ENSMUSG00000110439	chr4:60821320-60821418:-	Mm2
 #GF0000007.001	FBgn0264775	chr3R:15509753-15509899:-	Dme
-open (ECL, $i3);
+open (ECL, $infile2) || die "It cannot open the exon clusters file ($infile2)\n";
 while (<ECL>){
     chomp($_);
     @l=split(/\t/,$_);
@@ -55,8 +53,9 @@ while (<ECL>){
 	$excl{$l[0]}.=",".$idov;
     }  
 }
+close ECL;
 
-##
+## prepares score
 my @keys=keys(%excl);
 foreach $cl (@keys){
     @ne=split(/\,/,$excl{$cl});
@@ -79,8 +78,8 @@ foreach $cl (@keys){
 }
 
 ##GF0000007.001	FBgn0264775	chr3R:15509753-15509899:-	Dme
-open (OUT, ">$out");
-open (ECL, $i3);
+open (OUT, ">$outfile") || die "It cannot open the output file ($outfile)\n";
+open (ECL, $infile2) || die "It cannot open the exon file (again)\n";
 while (<ECL>){
     chomp($_);
     @l=split(/\t/,$_);
@@ -91,7 +90,8 @@ while (<ECL>){
     $memb=sprintf("%.2f",$memb);
     print OUT "$_\t$memb\n";
 }
-
+close ECL;
+close OUT;
 
 
 
