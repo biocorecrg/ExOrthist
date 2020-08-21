477bcb8953f796d1eb8bf756bee75f8f5f265da9
mirimia <mirimia@gmail.com>
2020-08-21 12:30:43 +0200

    formal changes and added a line to delete existing PART_XX folders for Get_Pre_cluster_exons.pl

--- bin/Get_Pre_cluster_exons.pl
+++ bin/Get_Pre_cluster_exons.pl
@@ -1,80 +1,91 @@
 #!/usr/bin/env perl
-#use warnings;
-#use strict;
+use warnings;
+use strict;
 
-##
-my $i1=$ARGV[0]; ##Gene cluster file
-my $i2=$ARGV[1]; ##Exons scores by species pair
-my $nc=$ARGV[2]; ##number of clusters in each part
-my $out=$ARGV[3]; ##Outfile name
+my $infile1=$ARGV[0]; ## Gene cluster file
+my $infile2=$ARGV[1]; ## Exons scores by species pair => Best_score_exon_hits_pairs.txt
+my $nc=$ARGV[2]; ## Number of clusters in each part => default 500
+my $outfile=$ARGV[3]; ## Outfile name per folder
 
-#GF000009	Hsa	ENSG00000257008	GPR142	OG0000061O=0,R=1,F>R
-my ($id1, $sp1, $sp2, $l, $id2, $n);
-open (INFILE,"$i1");
+my %cid;
+open (INFILE, $infile1) || die "It cannot open the input file ($infile)\n";
+# Format: GF000009	Hsa	ENSG00000257008	GPR142	OG0000061O=0,R=1,F>R
 while (<INFILE>){
     chomp($_);
-    @l=split(/\t/,$_);
+    my @l=split(/\t/,$_);
     $cid{$l[2]}=$l[0];
 }
-#BL00000	Sc0000095:751518-751620:-	CD14496	clodip2_s0015:466798-466932:+	Bla	Cdi
+close INFILE;
+
 my $tmp="tmp.out";
-open (TMP, ">$tmp");
-open (INFILE,"$i2");
-while (<INFILE>){
+open (TMP, ">$tmp") || die "Tt cannot open the temporary tmp.out";
+open (INFILE2, $infile2) || die "It cannot open the Exons scores by species pair ($infile2)\n";
+# Format: BL00000	Sc0000095:751518-751620:-	CD14496	clodip2_s0015:466798-466932:+	Bla	Cdi
+while (<INFILE2>){
     chomp($_);
-    @l=split(/\t/,$_);
-    if ($cid{$l[0]}){  
+    my @l=split(/\t/,$_);
+    if (defined $cid{$l[0]}){  
 	print TMP "$cid{$l[0]}\t$_\n";
-	
-    }elsif ($cid{$l[2]}){
+    } 
+    elsif (defined $cid{$l[2]}){
 	print TMP "$cid{$l[2]}\t$_\n";
     }
 }
+close INFILE2;
 close (TMP);
+
 system ("cat $tmp | sort -k1 > sorted_scores.out");
-$cid="NA";
-open (INFILE,"sorted_scores.out");
-while (<INFILE>){
+my $cid="NA"; 
+my ($p, $part, $cls);
+my %cid2;
+
+system "rm -r PART_*/" if (-e "PART_01");
+
+open (TEMP_INFILE, "sorted_scores.out") || die "It cannot open the temporary input sorted_scores.out\n";
+while (<TEMP_INFILE>){
     chomp($_);
-    @l=split(/\t/,$_);
-    if ($cid eq "NA"){
+    my @l=split(/\t/,$_);
+
+    if ($cid eq "NA"){ # first case
 	$cid2{$l[0]}=1;
 	$cid=$l[0];
 	$part="PART_01";
 	$p=1;
 	$cls=1;
 	system ("mkdir PART_01");
-	$out="$part/cls_$cid.tab";
-	open (SC, ">>$out");
+	$outfile="$part/cls_$cid.tab";
+	open (SC, ">>$outfile");
 	print SC "$l[0]|$l[1]|$l[2]|$l[5]\t$l[0]|$l[3]|$l[4]|$l[6]\n";
     }
     elsif (!$cid2{$l[0]}){
-	if ($cls<$nc){
-		$cls++;
-	   	$cid2{$l[0]}=1;
-		$cid=$l[0];
-		$out="$part/cls_$cid.tab";
-		open (SC, ">>$out");
-		print SC "$l[0]|$l[1]|$l[2]|$l[5]\t$l[0]|$l[3]|$l[4]|$l[6]\n";
+	if ($cls < $nc){
+	    $cls++;
+	    $cid2{$l[0]}=1;
+	    $cid=$l[0];
+	    $outfile="$part/cls_$cid.tab";
+	    open (SC, ">>$outfile");
+	    print SC "$l[0]|$l[1]|$l[2]|$l[5]\t$l[0]|$l[3]|$l[4]|$l[6]\n";
 	}
-	else {
-	   	$cls=1;
-		$p++;
-	   	$cid2{$l[0]}=1;
-		$cid=$l[0];
-		if ($p<=9) { $part="PART_0$p"; }
-		else { $part="PART_$p";    }	
-		system ("mkdir $part");
-		$out="$part/cls_$cid.tab";
-		open (SC, ">>$out");
-		print SC "$l[0]|$l[1]|$l[2]|$l[5]\t$l[0]|$l[3]|$l[4]|$l[6]\n";
+	else { # re-starts file
+	    $cls=1;
+	    $p++;
+	    $cid2{$l[0]}=1;
+	    $cid=$l[0];
+	    if ($p<=9) { $part="PART_0$p";}
+	    else { $part="PART_$p";}	
+	    close SC;
+	    system ("mkdir $part");
+	    $outfile="$part/cls_$cid.tab";
+	    open (SC, ">>$outfile");
+	    print SC "$l[0]|$l[1]|$l[2]|$l[5]\t$l[0]|$l[3]|$l[4]|$l[6]\n";
 	}
-
     }
     elsif ($cid2{$l[0]}){
 	print SC "$l[0]|$l[1]|$l[2]|$l[5]\t$l[0]|$l[3]|$l[4]|$l[6]\n";
     }
     
 }
+close TEMP_INFILE;
+close SC;
 print STDERR "Number of parts:\t$p\n\n";
 
