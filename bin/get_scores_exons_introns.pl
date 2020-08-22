8311fb3e1ccb1ac186288c999de648933a5b36ab
mirimia <mirimia@gmail.com>
2020-08-22 12:54:25 +0200

    various bug corrections based on warnings on get_scores_exons_introns.pl

--- bin/get_scores_exons_introns.pl
+++ bin/get_scores_exons_introns.pl
@@ -40,8 +40,8 @@ my ($e,$i);
 my %introns;
 my %exons;
 my %intpos;
-#Format: >ENSP00000379387|ENSG00000185219  144.0 200.2 232.0 274.2 311.2
 open (EXONE, "$f1") || die "Missing exint file species 1";
+#Format: >ENSP00000379387|ENSG00000185219  144.0 200.2 232.0 274.2 311.2
 while (<EXONE>){
     chomp($_);
     if ($_=~/\>/){
@@ -78,11 +78,12 @@ while (<EXTWO>){
     }			
 }
 close (EXTWO);
+
 ##GETTING GENOMIC COORDS OF INTRONS
-# Format: BL22196_evm0|BL22196	Sc0000025	-	Intron_1	366299-366745
 my @k;
 my ($posint1, $posint2);
-open (IN,"$i6")|| die "Missing intron pos species 1"; ###exon intron scores
+open (IN,"$i6")|| die "It cannot open intron positions for species 1 ($i6)\n"; #  Intron coords sp1 (Sp1_protein_ids_intron_pos_CDS.txt)
+# Format: BL22196_evm0|BL22196	Sc0000025	-	Intron_1	366299-366745
 while (<IN>){
     chomp($_);
     @line=split(/\t/,$_);
@@ -95,7 +96,7 @@ while (<IN>){
 	}
     }
 }
-open (IN,"$i7")|| die "Missing intron pos species 2"; ###exon intron scores
+open (IN,"$i7")|| die "It cannot open intron positions for species 2 ($i7)\n"; #  Intron coords sp1 (Sp2_protein_ids_intron_pos_CDS.txt) 
 while (<IN>){
     chomp($_);
     @line=split(/\t/,$_);
@@ -108,6 +109,7 @@ while (<IN>){
 	}
     }
 }
+
 ##PROCESSING EXON POSITION FILES
 my %exsc;
 my %insc;
@@ -119,7 +121,7 @@ my %sps;
 open (IN,"$i2")|| die "It cannot open exon intron scores file (Best_scores_pair_exons.txt)\n"; ###exon intron scores
 while (<IN>){
     chomp($_);	
-    @line=split(/\t+/,$_);##change of format
+    @line=split(/\t+/,$_); ##change of format
     #Exon	ENSMUSP00000000028|ENSMUSG00000000028	exon_10	236-275	chr16	18795918-18796037	-	ENSP00000397434|ENSG00000093009	NO_EXON_ALN
     #13	ENSDARP00000044293|ENSDARG00000056001	exon_28	1152-1188	chr10	33997813-33997923	+	1	ENSP00000261575|ENSG00000073910	1-3	2.70	5.41	34	91.89	ENSP00000261575|ENSG00000073910	exon_1	1-54	chr13	32768274-32768434	+	Dre	Hsa
     if ($line[2]=~/exon/ && $line[7]==1){
@@ -136,8 +138,8 @@ while (<IN>){
 	else {}
     }
 }
+open (IN,"$i3")|| die "It cannot open intron scores (Score_all_introns.txt)\n";
 # Format: GF000006	ENSP00000268043|ENSG00000140451	intron_4	324	1	ENSMUSP00000049046|ENSMUSG00000041064	intron_4	333	1	0	z,z	25	Hs2	Mm2
-open (IN,"$i3")|| die "Missing exon intron scores file";
 while (<IN>){
     chomp($_);	
     @line=split(/\t+/,$_);
@@ -151,6 +153,42 @@ while (<IN>){
 	#$insc{$id}=$line[0]."\t".$line[5]."\t".$line[6]."\t".$line[7]."\t".$line[8]."\t".$line[1]."\t".$line[2]."\t".$line[3]."\t".$line[4]."\t".$line[9];
     }
 }
+
+### Some exons without aln miss basic info (rescued 22/08/20)
+### Tries to gather the info from the same EXONS_DB as intron annotations
+my ($root_sp1)=$i6=~/(.+?)_protein_ids_intron_pos_CDS.txt/;
+my ($root_sp2)=$i7=~/(.+?)_protein_ids_intron_pos_CDS.txt/;
+open (EX_ANNOT_1, "$root_sp1"."_protein_ids_exons_pos.txt") || die "It cannot open the exon positions internally for Sp1 ($root_sp1"."_protein_ids_exons_pos.txt)\n";
+# Format: GB42164-PA|GB42164   exon_3   94-166   chr1  10316-10534  -
+while (<EX_ANNOT_1>){
+    chomp($_);
+    my @t=split(/\t/);
+    my $ex_id = "$t[0]#$t[1]"; # ProtID|GeneID#exon_2  
+    if (!defined $coords{$ex_id}){ # it only fills gaps
+	$coords{$ex_id}="$t[3]:$t[4]:$t[5]";
+    }
+    if (!defined $sps{$t[0]}){
+	$sps{$t[0]}=$s1;
+    }
+}
+close EX_ANNOT_1;
+
+open (EX_ANNOT_2, "$root_sp2"."_protein_ids_exons_pos.txt") || die "It cannot open the exon positions internally for Sp2 ($root_sp2"."_protein_ids_exons_pos.txt)\n";
+# Format: GB42164-PA|GB42164   exon_3   94-166   chr1  10316-10534  -
+while (<EX_ANNOT_2>){
+    chomp($_);
+    my @t=split(/\t/);
+    my $ex_id = "$t[0]#$t[1]"; # ProtID|GeneID#exon_2  
+    if (!defined $coords{$ex_id}){ # it only fills gaps
+	$coords{$ex_id}="$t[3]:$t[4]:$t[5]";
+    }
+    if (!defined $sps{$t[0]}){
+	$sps{$t[0]}=$s2;
+    }
+}
+close EX_ANNOT_2;
+
+##### Starts the actual processing and putting together
 my @keys;
 @keys=keys(%aln);
 my $el;
@@ -161,18 +199,19 @@ my $cr;
 my $cl;
 my $ne1;
 my $ne2;
-#$outf="Final_aln_scores_".$s1."_".$s2.".txt";
-##HEADER##
-open (OUT, ">$outf");
+#### Opens output file
+open (OUT, ">$outf") || die "It cannot open the output file ($outf)\n";
 print OUT "CID\tProt_query\tFeat_query\tCoords_query\tProt_subject\tFeat_subject\tCoords_subject\tScore_feature\tSp_query\tSp_subject\n";
 foreach $el (@keys){
     @tmp=split(/\#/,$el);
     $prots=$aln{$el};
-    print "$prots\n";
+#    print "$prots\n";
     my $ex1=0; my $ex2=0;
-    if ($exons{$tmp[0]}){ 
+    if ($exons{$tmp[0]}){ # $tmp[0] => ProtID|GeneID,  $exons{$tmp[0]} => Number of exons
 	$cl=$pid1{$tmp[0]}; 
-	if ($exons{$tmp[0]}==1){
+
+	### Puts together the exon scores
+	if ($exons{$tmp[0]}==1){ # only one exon
 	    $id=$tmp[0]."#exon_1#".$tmp[1];
 	    if ($exsc{$id}){		
 		print OUT "$exsc{$id}\n";
@@ -184,36 +223,56 @@ foreach $el (@keys){
 	}
 	else {
 	    $nex=$exons{$tmp[0]};
-	    for ($m=1; $m<$nex; $m++){
+	    for ($m=1; $m<$nex; $m++){ # loops through all exons in the gene
 		$id=$tmp[0]."#exon_".$m."#".$tmp[1];
 		$cr=$coords{$tmp[0]."#"."exon_".$m};
-		if ($exsc{$id}){
+		if ($exsc{$id}){ # if it has score, all good.
 		    print OUT "$exsc{$id}\n";
 		}		
-		else {
-		    # Some exons miss info. Even species info
+		else {  # if it does NOT have score (some missed info; added extra code above to get annotations)
 		    print OUT "$cl\t$tmp[0]\texon_$m\t$cr\t$tmp[1]\tNO_EXON_ALN\tNA\tNA\t$sps{$tmp[0]}\t$sps{$tmp[1]}\n";
 		}
+
+		### Now it gets the intron info
 		$id2=$tmp[0]."#intron_".$m."#".$tmp[1];
-		#print "####$id2###\n";
-		if ($insc{$id2}){
+		if ($insc{$id2}){ # if there is score, it's easy
 		    print OUT "$insc{$id2}\n";
 		}		
 		else {
 		    $ne1=$tmp[0]."#exon_".$m."#".$tmp[1]; 
 		    $ne2=$tmp[0]."#exon_".($m+1)."#".$tmp[1];
 		    $pint1=$tmp[0]."_intron_".$m;
-		    if (!$exsc{$ne1} && !$exsc{$ne2}){					
+		    
+		    if (!$exsc{$ne1} && !$exsc{$ne2}){ 
 			print OUT "$cl\t$tmp[0]\tintron_$m\t$intpos{$pint1}\t$tmp[1]\tNO_ALN\tNA\tNA\t$sps{$tmp[0]}\t$sps{$tmp[1]}\n";
 		    }
-		    elsif ($exsc{$ne1}<0.2 && !$exsc{$ne2}){
-			print OUT "$cl\t$tmp[0]\tintron_$m\t$intpos{$pint1}\t$tmp[1]\tNO_ALN\tNA\tNA\t$sps{$tmp[0]}\t$sps{$tmp[1]}\n";
+		    elsif ($exsc{$ne1} && !$exsc{$ne2}){
+			my @temp1=split(/\t/,$exsc{$ne1});
+			if ($temp1[7]<0.2){
+			    print OUT "$cl\t$tmp[0]\tintron_$m\t$intpos{$pint1}\t$tmp[1]\tNO_ALN\tNA\tNA\t$sps{$tmp[0]}\t$sps{$tmp[1]}\n";
+			}
+			else { # if the Aln is decent, it's assigned as no intron
+			    print OUT "$cl\t$tmp[0]\tintron_$m\t$intpos{$pint1}\t$tmp[1]\tNO_INTRON\tNA\t0\t$sps{$tmp[0]}\t$sps{$tmp[1]}\n";
+			}
 		    }
-		    elsif ($exsc{$ne1}<0.2 && $exsc{$ne2}<0.2){
-			print OUT "$cl\t$tmp[0]\tintron_$m\t$intpos{$pint1}\t$tmp[1]\tNO_ALN\tNA\tNA\t$sps{$tmp[0]}\t$sps{$tmp[1]}\n";
+		    elsif ($exsc{$ne1} && $exsc{$ne2}){
+			my @temp1=split(/\t/,$exsc{$ne1}); # the info for exon_X
+			my @temp2=split(/\t/,$exsc{$ne2}); # the info for exon_X+1
+			if ($temp1[7]<0.2 && $temp2[7]<0.2){
+			    print OUT "$cl\t$tmp[0]\tintron_$m\t$intpos{$pint1}\t$tmp[1]\tNO_ALN\tNA\tNA\t$sps{$tmp[0]}\t$sps{$tmp[1]}\n";
+			}
+			else {
+			    print OUT "$cl\t$tmp[0]\tintron_$m\t$intpos{$pint1}\t$tmp[1]\tNO_INTRON\tNA\t0\t$sps{$tmp[0]}\t$sps{$tmp[1]}\n";
+			}
 		    }
-		    elsif (!$exsc{$ne1} && $exsc{$ne2}<0.2){
-			print OUT "$cl\t$tmp[0]\tintron_$m\t$intpos{$pint1}\t$tmp[1]\tNO_ALN\tNA\tNA\t$sps{$tmp[0]}\t$sps{$tmp[1]}\n";
+		    elsif (!$exsc{$ne1} && $exsc{$ne2}){
+			my @temp2=split(/\t/,$exsc{$ne2}); # the info for exon_X+1  
+			if ($temp2[7]<0.2){
+			    print OUT "$cl\t$tmp[0]\tintron_$m\t$intpos{$pint1}\t$tmp[1]\tNO_ALN\tNA\tNA\t$sps{$tmp[0]}\t$sps{$tmp[1]}\n";
+			}
+			else {
+			    print OUT "$cl\t$tmp[0]\tintron_$m\t$intpos{$pint1}\t$tmp[1]\tNO_INTRON\tNA\t0\t$sps{$tmp[0]}\t$sps{$tmp[1]}\n";
+			}
 		    }
 		    else {
 			print OUT "$cl\t$tmp[0]\tintron_$m\t$intpos{$pint1}\t$tmp[1]\tNO_INTRON\tNA\t0\t$sps{$tmp[0]}\t$sps{$tmp[1]}\n";
