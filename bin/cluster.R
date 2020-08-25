1a034a4d640589827919f32b1d9f8fd1e9d279cf
Federica Mantica <fmantica@ant-login7.linux.crg.es>
2020-08-25 14:48:41 +0200

    Adding best reciprocals in membership score computation

--- bin/cluster.R
+++ bin/cluster.R
@@ -8,36 +8,67 @@ file2 <- args[2]
 library(igraph, quietly = TRUE, warn.conflicts = FALSE, verbose = FALSE)
 library(hashmap, quietly = TRUE, warn.conflicts = FALSE, verbose = FALSE)
 
-my_table = read.table(file1)
-my_matrix = as.matrix(my_table)
+my_table = read.table(file1, col.names=c("ID1", "ID2", "Best_reciprocal"))
+######## Generate graph #############
+my_matrix = as.matrix(my_table[,c("ID1", "ID2")]) #select columns with IDs
 my_edges = graph_from_edgelist(my_matrix, directed=TRUE) #create directed graph starting from input.
 my_clusters = cluster_edge_betweenness(my_edges) #graph with the best modularity score.
 my_final_clusters = as.data.frame(as.matrix(membership(my_clusters))); colnames(my_final_clusters) = "ClusterID" #communities
 
-#compute degrees necessary for the membership score.
+######## Compute values for membership score #############
+#Compute the total number of genes in clusters.
+all_IDs = c(best_reciprocal_df_ID1, best_reciprocal_df_ID2)
+TOT_genes_in_cluster = length(unique(sub(";.*", "", sub("\\|", ";", sub(".*;", "", sub("\\|", ";", all_IDs))))))
+
+#Create a dictionary with exonID, number of TRUE Best_reciprocal
+#I select the number of best reciprocals within the same exon cluster
+best_reciprocal_matrix = vector()
+for (my_exon in rownames(my_final_clusters)) {
+  my_cluster_id = my_final_clusters[my_exon,"ClusterID"]
+  all_exons_in_cluster = rownames(subset(my_final_clusters, ClusterID ==  my_cluster_id))
+  all_rec_hits = c(as.vector(subset(my_table, ID1==my_exon & Best_reciprocal == "TRUE")$ID2), as.vector(subset(my_table, ID2==my_exon & Best_reciprocal == "TRUE")$ID1))
+  rec_hits_in_cluster = length(all_rec_hits[all_rec_hits %in% all_exons_in_cluster])
+  best_reciprocal_matrix = rbind(best_reciprocal_matrix, c(my_exon, rec_hits_in_cluster))
+}
+best_reciprocal_df = as.data.frame(best_reciprocal_matrix); colnames(best_reciprocal_df) = c("ExonID", "Best_reciprocal_num")
+best_reciprocal_df$ExonID = as.vector(best_reciprocal_df$ExonID); best_reciprocal_df$Best_reciprocal_num = as.numeric(as.vector(best_reciprocal_df$Best_reciprocal_num))
+
+#Create a dictionary with species -> number of species genes in the cluster.
+gene_species_vector = unique(sub("\\|.*\\|", ";", sub(".*;", "", sub("\\|", ";", all_IDs))))
+gene_species_count_df = as.data.frame(table(sub(".*;", "", gene_species_vector))); colnames(gene_species_count_df) = c("Species", "Freq")
+gene_species_count_df$Species = as.vector(gene_species_count_df$Species)
+
+####Compute the values necessary for the membership score.
 my_sizes_df = as.data.frame(sizes(my_clusters)); colnames(my_sizes_df) = c("ClusterID", "Freq") #number of exons in each community
 my_out_degrees_df = as.data.frame(as.matrix(degree(my_edges, mode = "out"))) #direct connections of each exon
 my_in_degrees_df = as.data.frame(as.matrix(degree(my_edges, mode = "in"))) #reciprocal hits
-#compute the number of exons for each species in each community
+#Compute the number of exons for each species in each community
 all_ids = paste0(gsub(".*\\|", "", rownames(my_final_clusters)), "_", my_final_clusters$ClusterID)
 all_species_counts_df = as.data.frame(table(gsub(".*\\|", "", all_ids))); colnames(all_species_counts_df) = c("Species_ClusterID", "Freq"); 
 all_species_counts_df$Species_ClusterID = as.vector(all_species_counts_df$Species_ClusterID) #transform factor to vector for hashmap to work
 
-#create hashes for translation
+#Create hashes for translation
+gene_species_count_dict = hashmap(gene_species_count_df$Species, gene_species_count_df$Freq)
+best_reciprocal_dict = hashmap(best_reciprocal_df$ExonID, best_reciprocal_df$Best_reciprocal_num)
 out_degree_dict = hashmap(rownames(my_out_degrees_df), my_out_degrees_df$V1)
 in_degree_dict = hashmap(rownames(my_in_degrees_df), my_in_degrees_df$V1)
 species_counts_dict = hashmap(all_species_counts_df$Species, all_species_counts_df$Freq)
 sizes_dict = hashmap(my_sizes_df$ClusterID, my_sizes_df$Freq)
 
-#compute membership score and save to final file.
-my_final_clusters$out_degree = out_degree_dict$find(rownames(my_final_clusters))
-my_final_clusters$in_degree = in_degree_dict$find(rownames(my_final_clusters))
-my_final_clusters$SPECIES_exons_in_cluster = species_counts_dict$find(paste0(gsub(".*\\|", "", rownames(my_final_clusters)), "_", my_final_clusters$ClusterID))
-my_final_clusters$TOT_exons_in_cluster = sizes_dict$find(my_final_clusters$ClusterID)
-my_final_clusters$membership_score = (my_final_clusters$in_degree + my_final_clusters$out_degree)/(2*(my_final_clusters$TOT_exons_in_cluster - my_final_clusters$SPECIES_exons_in_cluster))
+#Operate translations from hashes
 my_final_clusters$ExonID = rownames(my_final_clusters)
+my_final_clusters$Out_degree = out_degree_dict$find(my_final_clusters$ExonID)
+my_final_clusters$In_degree = in_degree_dict$find(my_final_clusters$ExonID)
+my_final_clusters$SPECIES_exons_in_cluster = species_counts_dict$find(paste0(gsub(".*\\|", "", my_final_clusters$ExonID), "_", my_final_clusters$ClusterID))
+my_final_clusters$TOT_exons_in_cluster = sizes_dict$find(my_final_clusters$ClusterID)
+my_final_clusters$N_reciprocals = best_reciprocal_dict$find(my_final_clusters$ExonID)
+my_final_clusters$SPECIES_genes_in_cluster = gene_species_count_dict$find(sub(".*\\|", "", my_final_clusters$ExonID))
 
+######## Compute membership score #############
+my_final_clusters$membership_score = (my_final_clusters$In_degree + my_final_clusters$Out_degree + my_final_clusters$N_reciprocals)/(2*(my_final_clusters$TOT_exons_in_cluster - my_final_clusters$SPECIES_exons_in_cluster) + 
+                                                                                                                                       (TOT_genes_in_cluster - my_final_clusters$SPECIES_genes_in_cluster))
 #reorder columns
-my_final_clusters = my_final_clusters[,c("ExonID", "ClusterID", "out_degree", "in_degree", "SPECIES_exons_in_cluster", "TOT_exons_in_cluster", "membership_score")]
+my_final_clusters = my_final_clusters[,c("ExonID", "ClusterID", "Out_degree", "In_degree", "SPECIES_exons_in_cluster", "TOT_exons_in_cluster", "N_reciprocals", "membership_score")]
 
+#save to file
 write.table(my_final_clusters, file=file2, quote=FALSE, sep="\t", row.names = FALSE)
