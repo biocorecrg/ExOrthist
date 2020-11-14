447efed6a7bb4ff555201d5fb93422a43f2a1ced
mirimia <mirimia@gmail.com>
2020-11-14 12:16:58 +0100

    more comments in C1

--- bin/C1_score_and_select_best_EX_match_by_targetgene.pl
+++ bin/C1_score_and_select_best_EX_match_by_targetgene.pl
@@ -10,11 +10,8 @@ my (@i1, @i2, @i1b, @i2b, @line, @t1, @t2, @t3, @t4, @name, @l1, @l2);
 my (%sintron);
 my (%psc, %esc, %isc, %cid, %le, %sps);
 
-open (IN, $input_file) || die "It cannot open File with Aln scores (Final_aln_scores_Sp1_Sp2.txt)\n";
+open (IN, $input_file) || die "It cannot open File with Aln scores (all_PROT_EX_INT_aln_features_Sp1-Sp2.txt)\n";
 ### Format (multi-line: various formats in a single file
-# (1) EXONS   # Exon	PAC30764619|Ocbimv22009530m	exon_1	1-33	Scaffold1679	955644-955741	+	ENSP00000403502|ENSG00000178035	1-33	78.79%	78.79%	0	0.00%	ENSP00000403502|ENSG00000178035	exon_1	1-33	chr3	49066686-49066783	-
-# (2) INTRONS # CID:6948	PAC30764619|Ocbimv22009530m	intron_1	33	phase_1	ENSP00000403502|ENSG00000178035	intron_1	33	phase_1	conserved;u,u
-# (3) PROTEIN # CID:6948	Protein	Query:PAC30764619|Ocbimv22009530m	Subject:ENSP00000403502|ENSG00000178035	43.07%	81.36%	38.33%	0.29
 while (<IN>){
     chomp($_); 
     @line=split(/\t/,$_);
@@ -28,7 +25,7 @@ while (<IN>){
 	$le{$line[1]."#".$line[4]}=$line[2]; ##saving last exon
     }
     elsif ($line[2]=~/intron/){ # (2)
-	$id=$line[1]."#".$line[2]."#".$line[4];
+	$id=$line[1]."#".$line[2]."#".$line[4]; 
 	if ($line[5] eq "NO_ALN"){ }
 	elsif ($line[5] eq "NO_INTRON"){ $isc{$id}=$line[5]; }
 	else {
@@ -37,15 +34,19 @@ while (<IN>){
 		my $ps;
 		for ($ps=1; $ps<=10; $ps++){ 	
 		    if($line[7]==$ps) { $isc{$id}=($ps*0.025); } ##change here now is max score = 10
+		    # if there are gaps, longer distance is allowed
+		    # Add a hash name $intron_N_hit{$id} = $line[5]; # stores intron N in Sp2
 		}
 	    }
 	}
     } 
     ### (3) not used
 }
+close IN;
 
+# open all_PROT_EX_INT_aln_features_Sp1-Sp2.txt again
 open (IN, $input_file) || die "It cannot open again File with Aln scores (Final_aln_scores_Sp1_Sp2.txt)\n";
-#my $scorefile=$outf."/All_scores_by_exon.txt";
+#my $scorefile=$outf."/All_scores_by_exon.txt"; # old name
 my $scorefile=$outf."/all_scored_EX_matches.txt";
 open (OUTONE, ">$scorefile");
 print OUTONE "CID\tFeature\tExon_prot_loc\tProt_query\tExon_number_query\tExon_coords_query\tProt_Subject\tExon_number_subject\tExon_coords_subject";
@@ -117,10 +118,11 @@ while (<IN>){
 	else { ##internal exon
 	    @l1=split(/\_/,$line[2]);
 	    $e1=$line[1]."#exon_".($l1[1]-1)."#".$line[4];
-	    $i1=$line[1]."#intron_".($l1[1]-1)."#".$line[4];
-	    $e2=$line[1]."#exon_".($l1[1]+1)."#".$line[4];
-	    $i2=$line[1]."#intron_".($l1[1])."#".$line[4];
-	    if ($isc{$i1}) { if ($isc{$i1} eq "NO_INTRON"){ $sI1=0; } else { $sI1=$isc{$i1}; }  } else { $sI1=-0.25; }
+	    $i1=$line[1]."#intron_".($l1[1]-1)."#".$line[4]; # intron -1 in Sp1
+	    $e2=$line[1]."#exon_".($l1[1]+1)."#".$line[4];   
+	    $i2=$line[1]."#intron_".($l1[1])."#".$line[4];   # intron +1 in Sp1
+	    # add a check for Sp2 introns -1 and +1 being consecutive
+	    if ($isc{$i1}) { if ($isc{$i1} eq "NO_INTRON"){ $sI1=0; } else { $sI1=$isc{$i1}; }  } else { $sI1=-0.25; } 
 	    if ($esc{$e1}) { $sC1=($esc{$e1} * 0.15); } # prev = 0.16
 	    if ($isc{$i2}) { if ($isc{$i2} eq "NO_INTRON") { $sI2=0; } else { $sI2=$isc{$i2}; } } else{ $sI2=-0.25; }
 	    if ($esc{$e2}) { $sC2=($esc{$e2} * 0.15); } # prev = 0.16 
