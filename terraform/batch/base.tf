9e0f8d1170f3450917bfc2beb99c890523c1e137
Toni Hermoso Pulido <toniher@cau.cat>
2021-02-11 15:34:40 +0100

    starting infra

---
+++ terraform/batch/base.tf
@@ -0,0 +1,34 @@
+// Credentials to use. Here in Frankfurt region
+
+provider "aws" {
+  access_key = "yourkeyhere"
+  secret_key = "yoursecrethere"
+  region     = "eu-central-1"
+}
+
+
+// You may define an entry point for convenience
+
+resource "aws_instance" "entrypoint" {
+ 
+  ami         = "ami-06a3c664bd6bb3fce"
+  instance_type = "t2.micro"
+  iam_instance_profile = "S3access"
+  key_name = "key-nf"
+  security_groups = [ "allow_ssh" ]
+  tags = {
+	name = "entrypoint"
+  }
+
+}
+
+resource "aws_s3_bucket" "exorthist-nf" {
+  bucket = "exorthist-nf"
+  acl    = "private"
+  force_destroy = true
+  
+  tags = {
+    name = "S3 for ExOrthist"
+  }
+}
+
