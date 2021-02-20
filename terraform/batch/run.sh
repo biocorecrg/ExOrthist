47e722d2b98f7483b380886ce0493963803a2365
Toni Hermoso Pulido <toniher@cau.cat>
2021-02-20 13:36:56 +0100

    More details

--- terraform/batch/run.sh
+++ terraform/batch/run.sh
@@ -4,4 +4,4 @@ git clone https://github.com/biocorecrg/ExOrthist
 cd ExOrthist
 git checkout aws
 
-nextflow run main.nf -with-singularity -bg -config params.config.test -profile awsbatch -with-report -with-trace > /mnt/data-nf/log.txt
+nextflow run main.nf -with-singularity -bg -config params.config.test -profile awsbatch -with-report -with-trace > /mnt/s3/data/log.txt
