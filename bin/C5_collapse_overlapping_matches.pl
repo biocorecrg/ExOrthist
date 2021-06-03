10e7c7359f08aa82cb6ec153f1f5e40fd148356b
mirimia <mirimia@gmail.com>
2021-06-03 17:11:29 +0200

    comments to C5

--- bin/C5_collapse_overlapping_matches.pl
+++ bin/C5_collapse_overlapping_matches.pl
@@ -4,7 +4,7 @@ use strict;
 
 ##SCRIPT TO GET RIGD OF REDUNDANT PAIR OF EXONS##
 my $infile1=$ARGV[0]; ## overlapping exon info (Overlap_exons_by_sp.tab)
-my $infile2=$ARGV[1]; ## best score hit exons (Best_score_exon_hits_filtered_$m-$int-$id.tab)
+my $infile2=$ARGV[1]; ## best score hit exons (filtered_best_scored_EX_matches_by_targetgene.tab);
 my $outfile=$ARGV[2]; ## outfile name
 my $infile3=$ARGV[3]; ## liftover pairs, if provided
 
@@ -16,10 +16,10 @@ open (IN, $infile1) || die "It cannot open overlapping exons info ($infile1)\n";
 while (<IN>){
     chomp($_);
     my @l=split(/\t/,$_);
-    $exid{$l[1]."\t".$l[2]}=$l[0];
+    $exid{$l[1]."\t".$l[2]}=$l[0]; # individual_exon => Exon_group_ID
     if (!$max{$l[0]}){
 	$max{$l[0]}=$l[4];
-	$bh{$l[0]}=$l[1]."\t".$l[2];
+	$bh{$l[0]}=$l[1]."\t".$l[2]; # exon_group_ID => individual_exon
     }
     elsif ($l[4]>$max{$l[0]}) {
 	$max{$l[0]}=$l[4];
@@ -30,14 +30,14 @@ close IN;
 
 open (OUT, ">$outfile") || die "It cannot open the output file $outfile\n";
 open (IN2, $infile2) || die "It cannot open fil with best score exons ($infile2)\n"; 
-## Format: BL00000	Sc0000095:736652-736717:-	NEMVEDRAFT_v1g239017	NEMVEscaffold_11:253907-253972:-	Bla	Nve SCORE?
+## Format: BL00000	Sc0000095:736652-736717:-	NEMVEDRAFT_v1g239017	NEMVEscaffold_11:253907-253972:-	Bla	Nve
 <IN2>; # discards header
 while (<IN2>){
     chomp($_);
     my @l=split(/\t/,$_);
     my $id1=$l[0]."\t".$l[1];
-    my $ovid1=$exid{$id1};
-    my $bh1=$bh{$ovid1};
+    my $ovid1=$exid{$id1}; # Gets exon_group_ID
+    my $bh1=$bh{$ovid1};  # best representative of the exon_group_ID
     my $id2=$l[2]."\t".$l[3];
     my $ovid2=$exid{$id2};
     my $bh2=$bh{$ovid2};
